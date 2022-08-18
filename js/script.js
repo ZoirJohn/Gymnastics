d2131c6e593adb1faf243990d3e9f6377ac89d58
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2022-08-18 18:40:58 +0500

    start the work

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+console.clear();
+// Magic header
+// const  = document.querySelector('');
+// window.addEventListener('scroll', fixing);
+// function fixing() {
+//    if (scrollY <= .getBoundingClientRect().height * 2) {
+//       .style.transform = `translate(0,-${scrollY}px)`;
+//       .classList.remove('p');
+//    } else {
+//       .classList.add('p');
+//    }
+// }
+
+
+// const parallaxItemsXY = document.querySelectorAll('[data-depth]');
+// const parallaxItemsXorY = document.querySelectorAll('[data-depthX]');
+
+// function parallax(nodelist) {
+//    if (nodelist.length >= 0) {
+//       nodelist.forEach((item) => {
+//          const parent = item.closest('#page');
+//          parent.addEventListener('mousemove', parallaxOnMove);
+//       });
+//    }
+
+//    function parallaxOnMove(e) {
+//       const x = e.x;
+//       const y = e.y;
+//       nodelist.forEach((item) => {
+//          if (item.dataset.depthy && item.dataset.depthx && item.dataset.transition) {
+//             item.style.transform = `translate(${x * item.dataset.depthx * 0.1}px,${y * item.dataset.depthy * 0.1}px)`;
+//             item.style.transition = `all ${item.dataset.transition}ms ease-out`;
+//          } else if (item.dataset.depth && item.dataset.transition) {
+//             item.style.transform = `translate(${x * item.dataset.depth * 0.1}px,${y * item.dataset.depth * 0.1}px)`;
+//             item.style.transition = `all ${item.dataset.transition}ms ease-out`;
+//          }
+//       });
+//    }
+// }
+// parallax(parallaxItemsXY);
+// parallax(parallaxItemsXorY);
